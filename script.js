a6c5efab77df9ba92456ab3a85e725bd40399aef
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-10-07 22:58:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-//your JS code here. If required.
-function removeColore(){
-	let colorSelect=document.getElementByID("colorSelect");
-	let selectIndex=colorSelect.selectIndex;
-	if(selectIndex !==-1)
-	{
-		colorSelect.remove(selectIndex);
-		
-		
-	}
+function removeColor() {
+    // Get the dropdown element
+    let colorSelect = document.getElementById("colorSelect");
+    
+    // Get the index of the selected option
+    let selectedIndex = colorSelect.selectedIndex;
+
+    // If a valid option is selected, remove it
+    if (selectedIndex !== -1) {
+        colorSelect.remove(selectedIndex);
+    }
 }
+
+// Attach the function to the button when the page is fully loaded
 window.onload = function() {
     // Get the button and attach the click event
     let removeButton = document.querySelector('input[type="button"]');
