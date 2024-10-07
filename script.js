7b5f1c8e1d828e598a2200367763b3782a707325
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-10-07 22:57:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function removeColore(){
+	let colorSelect=document.getElementByID("colorSelect");
+	let selectIndex=colorSelect.selectIndex;
+	if(selectIndex !==-1)
+	{
+		colorSelect.remove(selectIndex);
+		
+		
+	}
+}
+window.onload = function() {
+    // Get the button and attach the click event
+    let removeButton = document.querySelector('input[type="button"]');
+    removeButton.addEventListener('click', removeColor);
+};
